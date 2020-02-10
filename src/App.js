03cfa4f8969851a1bac220d5a7b434c9146c5b94
Katie Wokasch <katie.wokasch@gmail.com>
2020-02-09 17:23:26 -0700

    Commit first attempt

--- src/App.js
+++ src/App.js
@@ -1,26 +1,100 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    domainNames: [],
+    currentDomainNames: '',
+    results: [
+      {
+          domainName: "example.org",
+          sld: "example",
+          tld: "org"
+      },
+      {
+          domainName: "google.com",
+          sld: "google",
+          tld: "com"
+      },
+      {
+        domainName: "timounkontan.org",
+        sld: "timounkontan",
+        tld: "org",
+        purchasable: true,
+        purchasePrice: 12.99,
+        purchaseType: "registration",
+        renewalPrice: 12.99
+      }
+    ]
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+
+    const domainNames = this.state.currentDomainNames.split(', ')
+    
+    this.setState({ domainNames })
+
+    this.postNames({ domainNames })
+  }
+
+  handleChange = event => {
+    let currentDomainNames = this.state.currentDomainNames
+    currentDomainNames = event.target.value
+    this.setState({ currentDomainNames })
+  }
+
+  postNames = domainNames => {
+    console.log(domainNames)
+
+    // fetch('https://api.dev.name.com/v4/domains:checkAvailability', {
+    fetch('http://api.dev.name.com/v4/domains:checkAvailability', {
+      method: 'POST',
+      headers: {
+        'Authorization': 'Basic a3dva2FzY2gtdGVzdDo1NzJjNTI0NDUyM2MzYWNjMzEyM2ExZDUyMDYwNDE3MDA5YWFiNjg0',
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify(domainNames)
+    }).then(response => response.json())
+    .then(res => console.log(res))
+  }
+
+  resultsMap = results => {
+    results.map(result => {
+      return <h2>{result.domainName}</h2>
+    })
+  }
+  render(){
+    return (
+      <div className="App">
+        <h2>Domain Search:</h2>
+        <form onSubmit={this.handleSubmit}>
+          <label>Domain Name/s:</label>
+          <input type='text' name='domainName' placeholder='google.com, etc.' onChange={this.handleChange}/>
+          <input id='submit' type='submit' value="Submit"/>
+        </form>
+        <div>
+          <h2>Search Results</h2>
+          {this.state.results.map(result => {
+              return <div>
+                
+                {
+                  result.purchasable
+                  ? <div>
+                      <h3>'{result.domainName}' is available!</h3>
+                      <h5>Purchase for ${result.purchasePrice}!</h5>
+                      <h5>Purchase Type: {result.purchaseType}</h5>
+                    </div>
+                  : <h3>'{result.domainName}' is not available.</h3>
+                }
+              </div>
+            })
+          }
+        </div>
+        
+      </div>
+    );
+  }
 }
 
-export default App;
